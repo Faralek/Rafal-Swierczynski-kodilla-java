8c5bd6a10a1209515102d1913185c9008f789c4b
Faralek <swierczynski093@gmail.com>
2020-10-16 15:34:29 +0200

    module 14.4

---
+++ kodilla-spirng-web/src/main/java/com/kodilla/spirng/web/KodillaSpirngWebApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.spirng.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@springbootapplication
+public class KodillaSpirngWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaSpirngWebApplication.class, args);
+	}
+
+}
