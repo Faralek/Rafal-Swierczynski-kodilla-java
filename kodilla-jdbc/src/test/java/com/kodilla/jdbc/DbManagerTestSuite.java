1f1b9c15c6b7c13479808bf78213dbdc8ce7dea1
48727 <swierczynski093@gmail.com>
2020-12-13 22:09:29 +0100

    module 30.3

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -60,6 +60,7 @@ public class DbManagerTestSuite {
         }
         rs.close();
         statement.close();
-        Assert.assertEquals(2, counter);
+        Assert.assertEquals(1, counter);
     }
+
 }
---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,33 @@
+package com.kodilla.jdbc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StoredProcTestSuite {
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"NOT SET\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //when
+        String sqlProcedureCall = "Call updateVipLevels()";
+        statement.execute(sqlProcedureCall);
+        //then
+        String sqlCheckTable = "Select count(*) as HOW_MANY FROM READERS WHERE VIP_LEVEL=\"NOT SET\"";
+        ResultSet resultSet = statement.executeQuery(sqlCheckTable);
+
+        int howMany = -1;
+        if (resultSet.next()){
+            howMany = resultSet.getInt("HOW_MANY");
+        }
+        Assert.assertEquals(0, howMany);
+    }
+
+}
