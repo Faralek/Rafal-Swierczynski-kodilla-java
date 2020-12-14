bd735861ab83684671cb14add452e408726c6f04
48727 <swierczynski093@gmail.com>
2020-12-14 06:02:06 +0100

    Module 31.2 zadanie

--- kodilla-testing2/src/test/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/test/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,21 +1,19 @@
-package com.kodilla.testing2.google;
+package com.kodilla.testing2.ebay;
 
 import com.kodilla.testing2.config.WebDriverConfig;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class GoogleTestingApp {
+public class EBayTestingApp {
     public static final String SEARCHFIELD = "gh-ac";
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
-
-        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
-        driver.get("https://www.google.com");
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.ebay.com");
 
         WebElement search = driver.findElement(By.id(SEARCHFIELD));
-        search.submit();
         search.sendKeys("Laptop");
         search.submit();
 
